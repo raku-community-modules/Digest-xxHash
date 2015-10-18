91a8c23d2e893208479fbfbc7396eb3e6a8c6072
Andy Weidenbaum <archbaum@gmail.com>
2015-10-18 12:49:45 -0700

    made compat with rakudo 2015.10, allman braces

--- Build.pm
+++ Build.pm
@@ -2,33 +2,35 @@ use v6;
 use Panda::Common;
 use Panda::Builder;
 
-my $this-is-windows = $*OS eq 'MSWin32';
-
-class Build is Panda::Builder {
-        method build(Str $path) {
-                if $this-is-windows {
-                        note '                           _-';
-                        note '                         _-//';
-                        note '                      _-_/ /';
-                        note '                   _--_/ _/';
-                        note '                 _- _/ _/';
-                        note '          ___-(O) _/ _/  **************************';
-                        note '      __--  __   /_ /    * Please see the README  *';
-                        note '____--__----  /    \_    * for information on how *';
-                        note ' -----       /   \_  \_  * to install on windows. *';
-                        note '           //   // \__ \_**************************';
-                        note '         //   //      \_ \_';
-                        note '      ///   //          \__-';
-                        return;
-                }
-                my $string-to-execute = "make panda-install";
-                say "Executing: {$string-to-execute}";
-                my $retval = +shell $string-to-execute;
-                if $retval != 0  {
-                        note "*** xxhash library build failed with exit status {$retval}!";
-                        exit 1;
-                }
+class Build is Panda::Builder
+{
+    method build(Str $path)
+    {
+        if $*DISTRO.is-win
+        {
+            note '                           _-';
+            note '                         _-//';
+            note '                      _-_/ /';
+            note '                   _--_/ _/';
+            note '                 _- _/ _/';
+            note '          ___-(O) _/ _/  **************************';
+            note '      __--  __   /_ /    * Please see the README  *';
+            note '____--__----  /    \_    * for information on how *';
+            note ' -----       /   \_  \_  * to install on windows. *';
+            note '           //   // \__ \_**************************';
+            note '         //   //      \_ \_';
+            note '      ///   //          \__-';
+            return;
         }
+        my $string-to-execute = "make panda-install";
+        say "Executing: $string-to-execute";
+        my $retval = +shell $string-to-execute;
+        if $retval != 0
+        {
+            note "*** xxhash library build failed with exit status {$retval}!";
+            exit 1;
+        }
+    }
 }
 
-# vi: ft=perl6
+# vim: ft=perl6
--- t/01-compare-correct.t
+++ t/01-compare-correct.t
@@ -1,32 +1,35 @@
 use v6;
 use Test;
-
 use Digest::xxHash;
-
-my Str $file-for-tests = IO::Spec.catpath("", $*PROGRAM_NAME.IO.path.absolute.directory, "digest-from-file");
-die ">>> File: '" ~ $file-for-tests ~ "' doesn't exist (it should be distributed along with this Digest::xxHash archive)!"
-	unless $file-for-tests.IO ~~ :f;
 
 plan 4;
 
+my Str $file_for_tests = $*PROGRAM-NAME.IO.dirname ~ "/digest-from-file";
+
+unless $file_for_tests.IO ~~ :f
 {
-	is xxHash(""), 0x2CC5D05,
-		"digest from empty string is correct";
+    die ">>> File: '"
+        ~ $file_for_tests
+        ~ "' doesn't exist (it should be distributed along with this
+           Digest::xxHash archive)!"
 }
 
 {
-	is xxHash("dupa"), 0x1A47C09D,
-		"digest from string 'dupa' is correct";
+    is xxHash(""), 0x2CC5D05, "digest from empty string is correct";
 }
 
 {
-	is xxHash(file => $file-for-tests), 0x1A47C09D,
-		"digest from file content is correct";
+    is xxHash("dupa"), 0x1A47C09D, "digest from string 'dupa' is correct";
 }
 
 {
-	is xxHash(buf-u8 => Buf[uint8].new(0x64, 0x75, 0x70, 0x61)), 0x1A47C09D,
-		"digest from uint8 buffer is correct";
+    is xxHash(file => $file_for_tests), 0x1A47C09D,
+        "digest from file content is correct";
 }
 
-# vi: ft=perl6
+{
+    is xxHash(buf-u8 => Buf[uint8].new(0x64, 0x75, 0x70, 0x61)), 0x1A47C09D,
+        "digest from uint8 buffer is correct";
+}
+
+# vim: ft=perl6
